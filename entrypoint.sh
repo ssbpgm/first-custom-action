fb5b5c241a4a2648165c9e67eef921cc949a40a5
Pedro García Medina <pagm.tech@gmail.com>
2023-10-11 00:57:27 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 set -e
 
-# if keyword is found
-if echo "$*" | grep -i -q FIXED;
+# look for the GH environment variable $GITHUB_EVENT_PATH to tell if the script is running in the workflow environment (launched by an actual event) or locally (when we're testing the action)
+if [ -n "$GITHUB_EVENT_PATH" ];
+then
+    EVENT_PATH=$GITHUB_EVENT_PATH
+elif [ -f ./sample_push_event.json ];
+then
+    EVENT_PATH='./sample_push_event.json'
+
+    # flag telling the script is running locally
+    LOCAL_TEST=true
+else
+    echo "No JSON data to process! :("
+    exit 1
+fi
+
+# print environment variables set by the OS
+env
+
+# print the contents of the action payload
+jq . < $EVENT_PATH
+
+# using jq to extract the contents of the payload to determine if the KEYWORD argument is found in the commits or head commits messages
+if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
 then
     # do something
     echo "Found keyword."
